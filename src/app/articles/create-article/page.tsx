cdcf42872ba451b9f7128d6cbc85e8196622201f
LucaMawyin <lucamawyin@gmail.com>
2026-05-11 10:11:12 -0400

    Added validation in create-articles

--- src/app/articles/create-article/page.tsx
+++ src/app/articles/create-article/page.tsx
@@ -1,137 +1,27 @@
-"use client";
+import { cookies } from "next/headers";
+import { getDB } from "@/lib/db";
+import CreateClient from "./CreateClient";
+import { redirect } from "next/navigation";
 
-import { useState, useRef } from "react";
-import { useRouter } from "next/navigation";
-import { LoginResponse } from "@/lib/types";
-import Tile from "@/components/Tile"
-import Button from "@/components/Button";
+export default async function LoginPage() {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("session")?.value;
 
+    let isLoggedIn = false;
 
-export default function Create(){
-
-  const router = useRouter();
-
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [preview, setPreview] = useState<string | null>(null);
-  const [ imageFile, setImageFile ] = useState<File | null>(null);
-
-
-  // Handling thumbnail for post
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      setImageFile(file);
-      setPreview(URL.createObjectURL(file));
-    }
-  };
-
-  async function handleSubmit(e : React.FormEvent){
-    e.preventDefault();
-
-    // Data stored in one object
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("content", content);
-
-    if (imageFile) { 
-      formData.append("image", imageFile);
+    if (!token){
+      redirect("/login");
     }
 
-    const response = await fetch("/api/articles", {
-      method:"POST",
-      body:formData,
-    });
+    const db = await getDB();
 
-    // Successful publish reroutes to articles
-    if (response.ok) {
-      router.push("/articles");
-    } 
-
-    // Somehow access is gained but not logged in
-    else if (response.status == 401){
-      router.push("/login");
+    const session = await db.prepare(`
+        SELECT * FROM sessions WHERE token = ?
+    `).bind(token).first();
+    
+    if (!session){
+      redirect("/login");
     }
 
-    // Default
-    else {
-      const data = await response.json() as LoginResponse;
-      alert(data.error || "Failed to create article");
-    }
-
-  }
-
-  return (
-    <div 
-      className="flex justify-center items-center min-h-[90vh]"
-    >
-      <Tile 
-        title="Create a New Article"
-        disableHover={true}
-        className="lg:max-w-[40vw] max-w-full"
-        >
-        <form 
-          className="
-            w-full
-            flex flex-col
-            justify-center
-            gap-3"
-          onSubmit={handleSubmit}
-        >
-          <label htmlFor="title">Title</label>
-          <input 
-            id="title"  
-            type="text" 
-            name="title" 
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Write an interesting title" 
-          />
-
-          <label htmlFor="content">Content</label>
-          <textarea 
-            id="content" 
-            className="min-h-75"
-            name="content" 
-            onChange={(e) => setContent(e.target.value)}
-            placeholder="Start writing your article" />
-          
-          <label htmlFor="thumbnail">Thumbnail</label>
-          <input 
-            id="thumbnail"
-            type="file" 
-            accept="image/*" 
-            onChange={handleFileChange} 
-            ref={inputRef}
-            className="hidden"
-          />
-          <Button text="Select Image" variant="secondary" onClick={() => inputRef.current?.click()} />
-          {preview && (
-            <img src={preview} alt="Preview" />
-          )}
-          <div className="flex 
-            flex-col 
-            gap-2
-            sm:flex-row sm:items-center sm:self-end">
-            <Button 
-              text="Post" 
-              type="submit" 
-              className="w-full sm:w-32"
-            />
-            <Button
-              text="Cancel"
-              type="reset"
-              variant="secondary"
-              className="w-full sm:w-32"
-              onClick={() => {
-                router.push("/articles");
-              }}
-            />
-          </div>
-        </form>
-      </Tile>
-
-    </div>
-  )
+    return <CreateClient />;
 }
