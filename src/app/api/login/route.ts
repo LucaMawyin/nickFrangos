3faaf5b76d56be2cf09d6f7a8fc4ec8bb96f9743
LucaMawyin <lucamawyin@gmail.com>
2026-05-15 16:54:08 -0400

    Send email on login (Implement email verification)

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,6 +3,8 @@ import bcrypt from "bcryptjs";
 import { getDB } from "@/lib/db";
 import { User } from "@/lib/types";
 import { LoginBody } from "@/lib/types";
+import { Resend } from "resend";
+
 
 export async function POST(request : Request){
     try {
@@ -55,8 +57,10 @@ export async function POST(request : Request){
 
         const userAgent = request.headers.get("User-Agent") || "unknown";
 
+        // Getting user geo info
         const geo = await getGeoFromIp(userIP);
 
+        // Adding to DB
         await db.prepare(`
             INSERT INTO sessions (token, user_id, expires_at, ip_address, geo, user_agent)
             VALUES (?, ?, datetime('now', '+1 days'), ?, ?, ?)
@@ -71,6 +75,12 @@ export async function POST(request : Request){
             path: "/",
             maxAge: 60 * 60 * 24 * 7,
         });
+
+        try {
+            await sendLoginEmail(email, userIP, geo, userAgent);
+        } catch (e) {
+            console.error("Email failed", e);
+        }
 
         return res;
     }
@@ -95,4 +105,30 @@ async function getGeoFromIp(ip: string) {
     } catch (err) {
         return err;
     }
+}
+
+async function sendLoginEmail(
+    email: string,
+    ip: string,
+    geo: any,
+    userAgent: string
+) {
+
+    const token = process.env.RESEND_TOKEN
+
+    const resend = new Resend(token);
+
+    await resend.emails.send({
+        from: "NicholasFrangos.com <onboarding@resend.dev>",
+        to: email,
+        subject: "New login detected",
+        html: `
+            <h2>New Login Alert</h2>
+            <p><strong>IP:</strong> ${ip}</p>
+            <p><strong>Device:</strong> ${userAgent}</p>
+            <p><strong>Country:</strong> ${geo?.location?.country || "Unknown"}</p>
+            <p><strong>Region:</strong> ${geo?.location?.region || "Unknown"}</p>
+            <p>If this wasn't you, please reset your password immediately.</p>
+        `,
+    });
 }
